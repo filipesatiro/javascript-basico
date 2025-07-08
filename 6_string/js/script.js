6e8581c9caf7da74cc49e3f1c8a9278d345b4feb
Filipe <filipelipe202229@gmail.com>
2025-07-08 14:35:57 -0300

    aprendendo o basico sobre strings

---
+++ 6_string/js/script.js
@@ -0,0 +1,24 @@
+var nome = 'Filipe';
+console.log(nome);
+
+console.log(typeof nome);
+console.log(typeof 'Sd');
+
+var sobreNome = 'Satiro';
+var nomeCompleto = nome + " " + sobreNome;
+
+console.log(nomeCompleto);
+
+console.log(typeof "5.256.874");
+
+var frase = 'Está é uma frase complexa';
+console.log(frase);
+console.log(typeof frase);
+
+console.log(nome + " " + frase);
+
+document.write('Ele disse: "Olá"');
+document.write("Ele disse: 'oi'");
+
+console.log("Este número:" + nome);
+

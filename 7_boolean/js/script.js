318eb319bae6dacb6f71d5a59f68affe559bb763
Filipe <filipelipe202229@gmail.com>
2025-07-08 14:36:28 -0300

    O basico sobre verdadeiro e falso o famoso boolean

---
+++ 7_boolean/js/script.js
@@ -0,0 +1,18 @@
+var verdadeiro = true;
+
+console.log(verdadeiro);
+console.log(typeof verdadeiro);
+
+console.log(typeof true);
+console.log(typeof false);
+
+var falso = false;
+
+console.log(falso);
+console.log(typeof falso);
+
+// var false = 3;
+// var function = 3;
+// var true = 3;
+// palavras reservadas não pode no javascript!
+

44cb3988abdcd6ed717bc5aca59c4c8452064bb8
Filipe <filipelipe202229@gmail.com>
2025-07-08 14:34:27 -0300

    O basico de variáveis em javascript

---
+++ 4_variaveis/js/script.js
@@ -0,0 +1,37 @@
+var teste = 1;
+
+var nome = "joão"
+
+console.log(teste);
+
+teste = 'Matheus'
+
+console.log(teste);
+
+console.log(nome);
+
+/* No javascript não podemos usar palavras reservadas 
+para declarar variàveis 
+ Variáveis começando com números também não é
+ permitido em javascript
+
+$ e  underline  podem ser usados para declaras
+ variáveis  */
+// var @nome = 'Asd'
+// console.log(@nome)
+
+var meuPrimeiroNome = 'Filipe'; //camelCase
+console.log(meuPrimeiroNome);
+
+let testando  = 1;
+const ola = 2;
+
+console.log(testando);
+console.log(ola);
+
+var meuNome;    
+
+meuNome = "Filipe"
+
+console.log(meuNome);
+

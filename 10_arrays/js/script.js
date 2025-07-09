249b30a62ba2144d5cc7d049674962ceb7339c27
Filipe <filipelipe202229@gmail.com>
2025-07-09 17:39:30 -0300

    Aprendendo o basico sobre Arrays

---
+++ 10_arrays/js/script.js
@@ -0,0 +1,15 @@
+var arr = [5, "Filipe", true, {teste:1, teste:2}];    //  obj => {}
+    console.log(arr);
+
+var arr2 = [2,3,4,5];
+console.log(arr2);
+
+console.log(arr[1]);
+console.log(arr2[0]);
+
+arr[4] = 10;
+arr[0] = "mudando valor";
+
+console.log(arr);
+console.log(typeof arr)
+

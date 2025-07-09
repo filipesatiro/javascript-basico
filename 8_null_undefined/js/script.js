8cdd043bb0c6e7d7b1ea20fe4a7002d60c8ba6fa
Filipe <filipelipe202229@gmail.com>
2025-07-09 17:41:06 -0300

    Arquivo criado Null e Undefined

---
+++ 8_null_undefined/js/script.js
@@ -0,0 +1,14 @@
+//Hosting - içamento
+console.log(sobrenome);
+console.log(numero)
+
+var nome = null;
+var sobrenome = 'Satiro';
+console.log(nome);
+console.log(sobrenome);
+
+nome = 'Filipe';
+
+console.log('Filipe');
+
+var numero = 5;

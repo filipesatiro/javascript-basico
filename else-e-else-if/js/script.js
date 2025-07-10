43d7b8d543bddfbcdefe700c49f1952478472a09
Filipe <filipelipe202229@gmail.com>
2025-07-09 23:11:30 -0300

    aprendendo sobre if, else if e else em javascript

---
+++ else-e-else-if/js/script.js
@@ -0,0 +1,25 @@
+var nome = "Xavi";
+
+if(nome == "Pedro") {
+    console.log("O nome dele é Pedro");
+} else if(nome == "Filipe") {
+    console.log("O nome dele é Filipe")
+} else if(nome == "Xavi"){
+    console.log("O nome é Xavi")
+} else {
+    console.log("Ele possui outro nome!");
+}
+
+var idade = 19;
+
+if(idade > 20) {
+    console.log("Ele pode entrar na festa")
+}else if(idade >=18) {
+    console.log("Ele só pode entrar com autorização")
+}
+
+if(nome == "Filipe"){
+    console.log("teste")
+} else {
+    console.log("TESTANDO")
+}

4f304922dae3776cc05c00f17ab4358b3dac1fec
Filipe <filipelipe202229@gmail.com>
2025-07-09 22:54:08 -0300

    aprendendo o básico de comparacão com os operadores

---
+++ 12_operadores-de-comparacao/js/script.js
@@ -0,0 +1,31 @@
+var idade = 8;
+var possuiCarro = 1;
+
+
+if(idade >= 18) {
+    console.log("O usuário pode tirar a CNH");
+}
+
+if(idade <=17) {
+    console.log("O usuário não têm idade para ter CNH")
+}
+
+if(possuiCarro) {
+    console.log("O usuário já pode andar de carro!")
+}
+var nome = "Pedro";
+
+if(nome == "Filipe") {
+    console.log("O seu nome é Filipe");
+}
+if(nome != "Filipe") {
+    console.log("O nome não é Filipe");
+}
+
+if(20 > 10) {
+    console.log("Passou");
+
+}
+if(100 < 20) {
+    console.log("Passou 2")
+}

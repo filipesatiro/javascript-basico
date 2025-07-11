d6ed3fd4e3d101f7e89b7f1f7c4fc219588f5b80
Filipe <filipelipe202229@gmail.com>
2025-07-11 15:10:11 -0300

    Criado o arquivo Operadores Lógicos &&(AND)

---
+++ 15_operadores-logicos/js/script.js
@@ -0,0 +1,13 @@
+var idade = 17;
+var nome = "João"
+
+if(nome == "João" && idade == 16) {
+    console.log("O João pode entrar na aula de esgrima")
+} else {
+    console.log("Este usuário não está cadastrado na aula de esgrima")
+}
+if ((1 == 1 && 3 > 3) && true) {
+    console.log("Passou")
+} else if (nome == "João" && idade >= 14) {
+    console.log("Aqui passa")
+}

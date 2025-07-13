72dbe01e8cb2b551e3fa5c7ca8fa9bb21ba0d087
Filipe <filipelipe202229@gmail.com>
2025-07-13 14:42:42 -0300

    aprendendo um pouco do básico sobre escopo

---
+++ 24_escopo/js/script.js
@@ -0,0 +1,26 @@
+//escopo global
+var x = 1;
+//escopo global
+var y = 3;
+console.log(x, y);
+
+//escopo local
+function teste(){
+
+    var z = 0
+    console.log(z);
+   
+}
+teste();
+
+function testando() {
+    var z = 5;
+    console.log(z);
+}
+testando();
+
+if(true) {
+    var p = 1;
+
+}
+console.log(p);

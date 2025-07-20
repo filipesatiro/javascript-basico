247ba14c6e9e3aa839607f9724719dec585f2a24
Filipe <filipelipe202229@gmail.com>
2025-07-20 19:54:26 -0300

    Adicionado o arquivo removendo elementos com remove e removechild

---
+++ 37_removendo-elementos/js/script.js
@@ -0,0 +1,10 @@
+//removendo elemento filho
+var container = document.querySelector('#container');
+var p = document.querySelector('#container p');
+container.removeChild(p);
+
+//removendo o elemento
+var subtitle = document.querySelector(".subtitle");
+var paragrafo = document.querySelector('#paragrafo');
+subtitle.remove();
+paragrafo.remove();

7caebe3a86b3dd6e4c2dae1a33b3978d7351a49e
Filipe <filipelipe202229@gmail.com>
2025-07-20 15:57:39 -0300

    alterando o texto em html com javascript com textContent e InnerHTML

---
+++ 35_altrando-conteudo/js/script.js
@@ -0,0 +1,9 @@
+// selecionar o elemento
+var title = document.querySelector('#title');
+
+//innerHTML
+title.innerHTML = "Testando o texto alterado!";
+
+//textContent -> mais utilizado,recomendadoo e performado
+var subtitle = document.querySelector(".subtitle");
+subtitle.textContent = "Testando o TextContent";

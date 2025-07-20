8e87a64b42721aa700ce19af83642ebc43ed2ac7
Filipe <filipelipe202229@gmail.com>
2025-07-20 16:54:34 -0300

    Inserindo texto dentro do HTML com querySelector e createTextNode

---
+++ 36_criando-elementos/js/script.js
@@ -0,0 +1,20 @@
+//Inserir o elemento no body
+var novoParagrafo = document.createElement("p");
+console.log(novoParagrafo);
+
+var texto = document.createTextNode("Este é o contéudo do paragrafo");
+novoParagrafo.appendChild(texto);
+console.log(novoParagrafo);
+
+var body = document.querySelector("body");
+console.log(body);
+body.appendChild(novoParagrafo);
+
+//inserir em um container
+var container = document.getElementById("container");
+console.log(container);
+
+var el = document.createElement("span");
+el.appendChild(document.createTextNode("Texto do Spam"));
+console.log(el);
+container.appendChild(el);

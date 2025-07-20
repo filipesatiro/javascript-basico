92f0c171dc51aeaca5d580f61168382b5e930a14
Filipe <filipelipe202229@gmail.com>
2025-07-20 15:35:50 -0300

    aprendendo a selecionar elementos com querySelectorAll e querySelector com javascript básico

---
+++ 34_queryselector-querryselectorall/js/script.js
@@ -0,0 +1,19 @@
+var itensClasse = document.getElementsByClassName('Item');
+console.log(itensClasse);
+
+//querySelectorAll
+var itensQuery = document.querySelectorAll('#lista2 li');
+console.log(itensQuery);
+
+var itensQuery2 = document.querySelectorAll('#lista .Item');
+console.log(itensQuery2);
+
+//querySelector
+var itensQuery3 = document.querySelector('#lista');
+console.log(lista);
+
+var primeiraLista = document.querySelector('ul');
+console.log(primeiraLista);
+
+var span = document.querySelector('#paragrafo span');
+console.log(span);

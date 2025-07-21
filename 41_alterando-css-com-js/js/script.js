cac361de32946fc85b10493d7a2887b970af2efa
Filipe <filipelipe202229@gmail.com>
2025-07-21 15:34:41 -0300

    Aprendendo a inserir CSS com Javascript no texto e mudando tags com JS

---
+++ 41_alterando-css-com-js/js/script.js
@@ -0,0 +1,14 @@
+//seleciona o elemento
+var title = document.querySelector("#title");
+
+//adiciona estilo
+ title.style.color = "red";
+
+ //background-color
+ title.style.backgroundColor = "yellow"
+
+ //selecionar elemento
+ var subtitle = document.querySelector(".subtitle");
+
+ //adiciona varios estilos
+ subtitle.style.cssText = "color: blue; background-color: pink; font-size: 50px";

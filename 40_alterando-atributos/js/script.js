cd67c5e3fe5b97f1cb38a1db5b11a705d9fa9690
Filipe <filipelipe202229@gmail.com>
2025-07-21 15:11:49 -0300

    adicionando e removendo atributos com js

---
+++ 40_alterando-atributos/js/script.js
@@ -0,0 +1,15 @@
+//adicionar atributo
+var title = document.querySelector("#title");
+title.setAttribute("class", "testando-atributo");
+
+console.log(title);
+
+var btn = document.querySelector("#btn");
+btn.setAttribute("disabled", "disabled");
+
+var subtitle = document.querySelector(".subtitle");
+subtitle.setAttribute("id", "titulo2");
+
+//remover atributo
+ var lista = document.querySelector("#lista");
+ lista.removeAttribute("id");

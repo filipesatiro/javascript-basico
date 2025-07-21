7e987b86361add6963ddf4c29ddd7db9145b6784
Filipe <filipelipe202229@gmail.com>
2025-07-21 17:35:07 -0300

    aprendendo sobre setTimeout e setInterval e como utilizalos para uma chamado com tempo especificado

---
+++ 44_setTimeout-e-setInterval/js/script.js
@@ -0,0 +1,12 @@
+//setTemiout
+console.log("Antes do setTimeout");
+setTimeout(function(){
+    console.log("Testando o setTimeout");
+},2000);
+
+console.log("Depois do setTimeou");
+
+//setInterval
+setInterval(function(){
+    console.log("Testando setInterval");
+},2000);

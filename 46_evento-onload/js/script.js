53d92ac7f49fc7e46dfd1fd16560f9683476a06e
Filipe <filipelipe202229@gmail.com>
2025-07-21 18:38:46 -0300

    Criada a pasta onload onde aprendemos a hierarquia de carregamento da página

---
+++ 46_evento-onload/js/script.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+    console.log("Carregou o Dom");
+    var title2 = document.querySelector("#title");
+    console.log(title2);
+}
+
+console.log("Carregou o Js")
+
+var title = document.querySelector("#title");
+console.log(title);
